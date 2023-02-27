f7afab41581ec31e31ec0a0bb0fbaa4bf848986a
shalqarov <shalqarov01@gmail.com>
2023-02-27 19:20:24 +0600

    update: db connectipn

--- api/handler.go
+++ api/handler.go
@@ -1,33 +1,35 @@
 package api
 
 import (
+	"github.com/Shalqarov/student-crud/repository"
 	"github.com/gofiber/fiber/v2"
 )
 
-type StudentModel struct {
+type Handler struct {
+	repo repository.Repo
 }
 
-func (m *StudentModel) create(c *fiber.Ctx) error {
+func (h *Handler) create(c *fiber.Ctx) error {
 	// TODO
 	return nil
 }
 
-func (m *StudentModel) read(c *fiber.Ctx) error {
+func (h *Handler) read(c *fiber.Ctx) error {
 	// TODO
 	return nil
 }
 
-func (m *StudentModel) readCollection(c *fiber.Ctx) error {
+func (h *Handler) readCollection(c *fiber.Ctx) error {
 	// TODO
 	return nil
 }
 
-func (m *StudentModel) update(c *fiber.Ctx) error {
+func (h *Handler) update(c *fiber.Ctx) error {
 	// TODO
 	return nil
 }
 
-func (m *StudentModel) delete(c *fiber.Ctx) error {
+func (h *Handler) delete(c *fiber.Ctx) error {
 	// TODO
 	return nil
 }
