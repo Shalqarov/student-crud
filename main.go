9582ad7c5759762f8cd217ad10472dcb09cf1509
shalqarov <shalqarov01@gmail.com>
2023-02-27 18:48:51 +0600

    add: external libraries

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use(logger.New())
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("раз раз проверка")
+	})
+
+	app.Listen(":8080")
+}
