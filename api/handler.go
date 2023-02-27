b5a0a58b9d6916299365f64e3bc7a78d008813e8
shalqarov <shalqarov01@gmail.com>
2023-02-27 20:30:18 +0600

    add: few handlers

--- api/handler.go
+++ api/handler.go
@@ -1,6 +1,9 @@
 package api
 
 import (
+	"net/http"
+	"strconv"
+
 	"github.com/Shalqarov/student-crud/repository"
 	"github.com/gofiber/fiber/v2"
 )
@@ -10,17 +13,54 @@ type Handler struct {
 }
 
 func (h *Handler) create(c *fiber.Ctx) error {
-	// TODO
+	student := new(repository.Student)
+	err := c.BodyParser(&student)
+	if err != nil {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(
+			fiber.Map{
+				"errors": err.Error() + "; content-type must be 'application/json'",
+			})
+	}
+	err = h.repo.Create(student)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(
+			fiber.Map{
+				"errors": err.Error(),
+			})
+	}
+	c.Status(200).JSON(&fiber.Map{
+		"student": student,
+	})
+
 	return nil
 }
 
 func (h *Handler) read(c *fiber.Ctx) error {
-	// TODO
+	strId := c.Params("id")
+	id, err := strconv.Atoi(strId)
+	if err != nil || id <= 0 {
+		return c.Status(http.StatusBadRequest).JSON(&fiber.Map{
+			"error": "StatusBadRequest",
+		})
+	}
+	student, err := h.repo.FindOne(uint64(id))
+	if err != nil {
+		return c.Status(http.StatusNotFound).JSON(&fiber.Map{
+			"error": "student not found",
+		})
+
+	}
+	c.Status(http.StatusOK).JSON(&fiber.Map{
+		"student": student,
+	})
 	return nil
 }
 
 func (h *Handler) readCollection(c *fiber.Ctx) error {
-	// TODO
+	students, _ := h.repo.FindAll()
+	c.Status(http.StatusOK).JSON(&fiber.Map{
+		"students": students,
+	})
 	return nil
 }
 
